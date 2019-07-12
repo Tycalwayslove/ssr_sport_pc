5ee550e0644d3613f83404d7affef1013b6c5029
Tycalwayslove <1186446929@qq.com>
2019-07-12 23:16:09 +0800

    <feat> 页面修改

--- sport_pc/pages/index.vue
+++ sport_pc/pages/index.vue
@@ -156,29 +156,7 @@ export default {
           avatar: 'https://mat1.gtimg.com/sports/sportapp/liansai_logo/NBA.png'
         }
       ],
-      items4: [
-        { header: 'Today' },
-        {
-          avatar: 'https://cdn.vuetifyjs.com/images/lists/1.jpg',
-          title: 'Brunch this weekend?',
-          subtitle:
-            "<span class='text--primary'>Ali Connors</span> &mdash; I'll be in your neighborhood doing errands this weekend. Do you want to hang out?"
-        },
-        { divider: true, inset: true },
-        {
-          avatar: 'https://cdn.vuetifyjs.com/images/lists/2.jpg',
-          title: 'Summer BBQ <span class="grey--text text--lighten-1">4</span>',
-          subtitle:
-            "<span class='text--primary'>to Alex, Scott, Jennifer</span> &mdash; Wish I could come, but I'm out of town this weekend."
-        },
-        { divider: true, inset: true },
-        {
-          avatar: 'https://cdn.vuetifyjs.com/images/lists/3.jpg',
-          title: 'Oui oui',
-          subtitle:
-            "<span class='text--primary'>Sandra Adams</span> &mdash; Do you have Paris recommendations? Have you ever been?"
-        }
-      ],
+    
       date: new Date().toISOString().substr(0, 10),
       menu1: false,
       menu2: false
