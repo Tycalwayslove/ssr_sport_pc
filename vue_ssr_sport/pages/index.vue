06958627f021e988b2f8fe1c52478dedb75edaf3
Tycalwayslove <1186446929@qq.com>
2019-08-06 06:22:51 +0800

    <style>布局-基础布局

--- vue_ssr_sport/pages/index.vue
+++ vue_ssr_sport/pages/index.vue
@@ -19,16 +19,54 @@
         </div>
       </div>
     </div>
-    <div class="nav_top">
-      <a
-        v-for="(item, index) in navList"
-        :Key="index"
-        :href="item.path"
-        class="active"
-      >
-      </a>
+    <div class="nav-top-box">
+      <div class="nav-top">
+        <ul>
+          <li
+            v-for="(item, index) in navTopList"
+            :Key="index"
+            @click="handleClickTabTop(index)"
+          >
+            <a :class="topnum === index ? 'hover' : ''">
+              {{ item.name }}
+            </a>
+          </li>
+        </ul>
+      </div>
     </div>
-    <div class="content"></div>
+    <div class="content">
+      <div class="content-match-fixed">
+        <a href="#">今天</a>
+        <a href="#">明天</a>
+        <a href="#">后天</a>
+      </div>
+      <div class="content-left">
+        <div class="content-match">
+          <div class="sport-list">
+            这里是数据列表组件
+          </div>
+        </div>
+      </div>
+      <div class="content-right">
+        <div class="block">
+          <div class="block-title">
+            <h1>热门频道</h1>
+          </div>
+          <div class="block-content">
+            <a href="#">CCTV5</a>
+            <a href="#">CCTV5</a>
+            <a href="#">CCTV5</a>
+          </div>
+        </div>
+      </div>
+      <!-- 足球集锦 -->
+      <div class="match-block">
+        <div class="block-title">
+          <h1>足球比赛集锦录像</h1>
+          <a href="#">查看更多</a>
+        </div>
+      </div>
+    </div>
     <div class="footer"></div>
   </div>
 </template>
@@ -59,7 +97,27 @@ export default {
           name: 'NBA',
           path: '/nba'
         }
-      ]
+      ],
+      navTopList: [
+        {
+          name: '首页',
+          path: '/index'
+        },
+        {
+          name: '直播',
+          path: '/zhibo'
+        },
+        {
+          name: '集锦',
+          path: '/jijin'
+        }
+      ],
+      topnum: 1
+    }
+  },
+  methods: {
+    handleClickTabTop(index) {
+      this.topnum = index
     }
   }
 }
@@ -113,11 +171,113 @@ export default {
           border-bottom: #fff;
         }
       }
-      .nav_top {
-        width: 100%;
-        height: 48px;
-        background: black;
-        box-shadow: 0 2px 4px 0 rgba(0, 0, 0, 0.1);
+    }
+  }
+
+  .nav-top-box {
+    height: 48px;
+    background: #fff;
+    box-shadow: 0 2px 4px 0 rgba(0, 0, 0, 0.1);
+    .nav-top {
+      width: 1200px;
+      height: 48px;
+      margin: 0 auto;
+      ul {
+        li {
+          float: left;
+          margin-left: 70px;
+          margin-top: 12px;
+          & a {
+            font-size: 18px;
+            color: #000000;
+            line-height: 24px;
+            display: block;
+            cursor: pointer;
+            &:hover {
+              color: #dc2221;
+            }
+          }
+          .hover {
+            position: relative;
+            color: #dc2221;
+            &:after {
+              position: absolute;
+              content: '';
+              bottom: -7px;
+              left: 14px;
+              width: 12px;
+              height: 0;
+              border-bottom: 2px solid #dc2221;
+            }
+          }
+        }
+      }
+    }
+  }
+  .content {
+    width: 1200px;
+    height: auto;
+    margin: 10px auto 0;
+    position: relative;
+    .content-match-fixed {
+      background: #fff;
+      width: 80px;
+      position: absolute;
+      top: 0px;
+      left: 0;
+      padding-bottom: 24px;
+      & a {
+        display: block;
+        font: 16px/28px 'microsoft yahei';
+        color: #333;
+        width: 80%;
+        text-align: center;
+        margin: 18px auto 0;
+      }
+    }
+    .content-left {
+      width: 760px;
+      height: auto;
+      left: 90px;
+      background: #fff;
+      float: left;
+      position: relative;
+      .content-match {
+        height: auto;
+        margin: 0 20px;
+        min-height: 400px;
+      }
+    }
+    .content-right {
+      width: 340px;
+      height: auto;
+      float: right;
+      .block {
+        height: auto;
+        margin-bottom: 10px;
+        padding-bottom: 20px;
+        background: #fff;
+        .block-title {
+          height: 40px;
+          background: #017bd1;
+          & h1 {
+            float: left;
+            padding: 0 15px;
+            height: 40px;
+            font: 20px/40px 'microsoft yahei';
+            color: #fff;
+            text-align: center;
+            background: #dc2221;
+          }
+        }
+        .block-content {
+          & a {
+            font: 14px/14px 'microsoft yahei';
+            color: #333;
+            display: inline-block;
+            margin: 22px 0 0 22px;
+          }
+        }
       }
     }
   }
