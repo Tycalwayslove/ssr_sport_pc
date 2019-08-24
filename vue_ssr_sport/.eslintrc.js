9a72293f1d9ea4c1f15ca71662795330cbc90341
Tycalwayslove <1186446929@qq.com>
2019-08-24 09:26:59 +0800

    <eslint> noconsole

--- vue_ssr_sport/.eslintrc.js
+++ vue_ssr_sport/.eslintrc.js
@@ -1,24 +1,33 @@
+/*
+ * @Descripttion: 
+ * @Author: tangyouchao
+ * @Date: 2019-08-06 05:08:25
+ * @LastEditors: tangyouchao
+ * @LastEditTime: 2019-08-24 08:43:49
+ */
 module.exports = {
-  root: true,
-  env: {
-    browser: true,
-    node: true
-  },
-  parserOptions: {
-    parser: 'babel-eslint'
-  },
-  extends: [
-    '@nuxtjs',
-    'prettier',
-    'prettier/vue',
-    'plugin:prettier/recommended',
-    'plugin:nuxt/recommended'
-  ],
-  plugins: [
-    'prettier'
-  ],
-  // add your custom rules here
-  rules: {
-    'nuxt/no-cjs-in-config': 'off'
-  }
+    root: true,
+    env: {
+        browser: true,
+        node: true
+    },
+    parserOptions: {
+        parser: 'babel-eslint'
+    },
+    extends: [
+        '@nuxtjs',
+        'prettier',
+        'prettier/vue',
+        'plugin:prettier/recommended',
+        'plugin:nuxt/recommended'
+    ],
+    plugins: [
+        'prettier'
+    ],
+    // add your custom rules here
+    rules: {
+        'nuxt/no-cjs-in-config': 'off',
+        "no-console": 0,
+        indent: ['error', 2] // 4个空格缩进
+    }
 }
