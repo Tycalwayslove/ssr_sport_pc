6919c6e7831fa92b9e2eb03388fd285d28f5d8c8
Tycalwayslove <1186446929@qq.com>
2019-09-17 06:13:21 +0800

    <feat> 去掉eslint

--- vue_ssr_sport/.eslintrc.js
+++ vue_ssr_sport/.eslintrc.js
@@ -3,31 +3,31 @@
  * @Author: tangyouchao
  * @Date: 2019-08-06 05:08:25
  * @LastEditors: tangyouchao
- * @LastEditTime: 2019-08-25 10:36:31
+ * @LastEditTime: 2019-09-17 05:05:13
  */
 module.exports = {
-    root: true,
-    env: {
-        browser: true,
-        node: true
-    },
-    parserOptions: {
-        parser: 'babel-eslint'
-    },
-    extends: [
-        '@nuxtjs',
-        'prettier',
-        'prettier/vue',
-        'plugin:prettier/recommended',
-        'plugin:nuxt/recommended'
-    ],
-    plugins: [
-        'prettier'
-    ],
-    // add your custom rules here
-    rules: {
-        'nuxt/no-cjs-in-config': 'off',
-        "no-console": 0,
-        indent: ['error', 2] // 4个空格缩进
-    }
+    // root: true,
+    // env: {
+    //     browser: true,
+    //     node: true
+    // },
+    // parserOptions: {
+    //     parser: 'babel-eslint'
+    // },
+    // extends: [
+    //     '@nuxtjs',
+    //     'prettier',
+    //     'prettier/vue',
+    //     'plugin:prettier/recommended',
+    //     'plugin:nuxt/recommended'
+    // ],
+    // plugins: [
+    //     'prettier'
+    // ],
+    // // add your custom rules here
+    // rules: {
+    //     'nuxt/no-cjs-in-config': 'off',
+    //     "no-console": 0,
+    //     indent: ['error', 2] // 4个空格缩进
+    // }
 }
